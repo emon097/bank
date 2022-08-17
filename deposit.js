1f90921422629422748e77fb981ce6c46652e00d
emon097 <mdemonhossain9527@gmail.com>
2022-08-17 10:24:04 +0600

    deposit

--- deposit.js
+++ deposit.js
@@ -1,7 +1,18 @@
+// step-1
 document.getElementById("deposit-btn").addEventListener("click", function () {
+  // step-2
   const depositinput = document.getElementById("deposit-input");
   const newdepositString = depositinput.value;
   const newdeposit = parseFloat(newdepositString);
   //   step-3
   depositinput.value = "";
+
+  //   step-4
+  const deposittotal = document.getElementById("deposit-total");
+  const previousdepositString = deposittotal.innerText;
+  const previousdeposit = parseFloat(previousdepositString);
+
+  //   step-5
+  const currentDeposit = previousdeposit + newdeposit;
+  deposittotal.innerText = currentDeposit;
 });
